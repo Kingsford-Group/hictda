64973b76110d3f53ea435f471e137c48fcb32517
Natalie Sauerwald <nsauerwa@cs.cmu.edu>
2019-07-10 15:25:02 -0400

    code/README updated for revisions

--- HiC_TDA.py
+++ HiC_TDA.py
@@ -8,6 +8,7 @@ import time
 import sys
 import csv
 import argparse
+import networkx as nx
 
 def read_raw_HiC_data(file):
     resolution=re.split('[_.]',os.path.basename(file).strip())[1]
@@ -55,7 +56,7 @@ def read_raw_HiC_data_no_split(file,reso):
         data_frequency_matrix[int((x-temp_min)/resolution),int((y-temp_min)/resolution)]=freq
         data_frequency_matrix[int((y-temp_min)/resolution),int((x-temp_min)/resolution)]=freq
     #print(data_frequency_matrix)
-    return (resolution,data_frequency_matrix)
+    return (resolution,data_frequency_matrix,temp_min,temp_max)
 
 def split_TAD(freq_matrix,TAD_result_file,resol):
     TAD_matrix_list=[]
@@ -119,7 +120,7 @@ def TDA_func(distance_matrix, persfilename,skelfilename):
     #print(diag)
     #data_0_dim=np.array([list(diag[i][1]) for i in range(len(diag)) if diag[i][0]==0])
     #data_1_dim=np.array([list(diag[i][1]) for i in range(len(diag)) if diag[i][0]==1])
-    return (skeleton,diag)
+    return (skeleton,diag,fullpersinfo)
 
 def writeSkeletonToFile(skeleton, filename):
     with open(filename,'w') as f:
@@ -128,6 +129,7 @@ def writeSkeletonToFile(skeleton, filename):
             if len(simplex) > 1:
                 fwriter.writerow(simplex)
     print('wrote simplex skeleton to',filename)
+    return
 
 def writePersistencePairsToFile(perspairs, filename):
     with open(filename,'w') as f:
@@ -135,6 +137,7 @@ def writePersistencePairsToFile(perspairs, filename):
         for pers in perspairs:
             fwriter.writerow(pers)
     print('wrote persistence pairs to',filename)
+    return
 
 def randomlyPermuteDistMat(distance_matrix,flag=''):
     distmatsize = distance_matrix.shape
@@ -171,15 +174,94 @@ def writeDistMatToFile(distmat,filename):
         for row in distmat:
             fwriter.writerow(row)
     print('wrote distance matrix to',filename)
+    return
+
+def writeLoopToFile(path_data,filename):
+    with open(filename,'w')as f:
+        fwriter = csv.writer(f,delimiter='\t')
+        for path in path_data:
+            fwriter.writerow([path[0],"other edges with the same length:"+str(path[1])])
+            fwriter.writerow(path[2])
+    print('wrote the information of loops to ', filename)
+    return
+
+
+def generate_1_dim_simp_list_from_dist_mat(dist_matrix):
+    mat_dim = dist_matrix.shape[0]
+    dim_1_simp_list = [[i,j,dist_matrix[i,j]] for i in range(0,mat_dim-1) for j in range(i+1,mat_dim)]
+    dim_1_simp_list.sort(key=lambda x: [x[2],max(x[0],x[1]),min(x[0],x[1])])
+    return dim_1_simp_list
+
+def get_one_dim_persis(fullpersis):
+    persis_1_dim = []
+    persis_1_dim_dict = {}
+    for dim,birth,death,simp_pair in fullpersis:
+        if(dim==1):
+            persis_1_dim.append([birth,death,simp_pair[0]])
+            if(tuple(simp_pair[0]) not in persis_1_dim_dict.keys()):
+                persis_1_dim_dict[tuple(simp_pair[0])] = [[birth,death,simp_pair[1]]]
+            else:
+                persis_1_dim_dict[tuple(simp_pair[0])].append([birth,death,simp_pair[1]])
+    persis_1_dim.sort(key=lambda x: [x[0]-x[1]])
+    return persis_1_dim,persis_1_dim_dict
+
+def check_same_length_edge(index,length,dim_1_simp_list):
+    edge_num = 1
+    temp_index = index-1
+    all_simp = len(dim_1_simp_list)
+    while True:
+        if(dim_1_simp_list[temp_index][2]-length==0):
+            edge_num+=1
+            temp_index-=1
+            if(temp_index<0):
+                break
+        else:
+            break
+    temp_index = index+1
+    while True:
+        if(dim_1_simp_list[temp_index][2]-length==0):
+            edge_num+=1
+            temp_index+=1
+            if(temp_index>=all_simp):
+                break
+        else:
+            break
+    return edge_num
+
+def get_loop(reso,minimal_bin,dist_mat,dim_1_simp_list,persis_1_dim_list,output_filename):
+    loop_num = len(persis_1_dim_list)
+    path_info = []
+    for i in range(loop_num):
+        essen_edge = persis_1_dim_list[i][2]
+        essen_index = dim_1_simp_list.index([min(essen_edge[0],essen_edge[1]),max(essen_edge[0],essen_edge[1]),dist_mat[essen_edge[0],essen_edge[1]]])
+        #check if there are other edges having the same length as the essential edge
+        multi_edge = check_same_length_edge(essen_index,dist_mat[essen_edge[0],essen_edge[1]],dim_1_simp_list)
+        if(multi_edge>1):
+            print("essential edge: bin: %d bin: %d length: %f"%(int(minimal_bin+reso*essen_edge[0]),int(minimal_bin+reso*essen_edge[1]),dist_mat[essen_edge[0],essen_edge[1]]))
+            print("another %d edges have the same length as the essential edge..."%(multi_edge-1))
+        #get shortest path
+        G=nx.Graph()
+        G.add_weighted_edges_from(dim_1_simp_list[:essen_index])
+        path_info.append([[int(minimal_bin+reso*i) for i in essen_edge],multi_edge-1,[int(minimal_bin+reso*i) for i in nx.shortest_path(G, source=essen_edge[0], target=essen_edge[1],weight="weight")]])
+    writeLoopToFile(path_info,output_filename)
+    return
+
+
 
 def main(input_file,output_path,output_name,resol):
-    resolut,freq_mat=read_raw_HiC_data_no_split(input_file,resol)
-    print("generate distance matrix...")
+    t0 = time.time()
+    resolut,freq_mat,min_bin,_=read_raw_HiC_data_no_split(input_file,resol)
+    print("Generate distance matrix...")
     distance_matrix=matrix_normalize([freq_mat])[0]
     writeDistMatToFile(distance_matrix,output_path+output_name+'_distmat.txt')
-    t0=time.time()
-    simplex_skel,persisdiag=TDA_func(distance_matrix,output_path+output_name+'_persisdiagram.txt',output_path+output_name+'_skeleton.txt')
+    print("Calculate persistent homology...")
+    simplex_skel,persisdiag,full_persis_info=TDA_func(distance_matrix,output_path+output_name+'_persisdiagram.txt',output_path+output_name+'_skeleton.txt')
+    print("Loop trace back...")
+    persis_dim_1_list,_ = get_one_dim_persis(full_persis_info)
+    dim_1_simp = generate_1_dim_simp_list_from_dist_mat(distance_matrix)
+    get_loop(resolut,min_bin,distance_matrix,dim_1_simp,persis_dim_1_list,output_path+output_name+'_loop_information.txt')
     print('total time (s):',time.time()-t0)
+    return
 
 if __name__ == "__main__":
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Overview
 
-This repo contains the python source code to run the method described in "Topological data analysis reveals principles ofchromosome structure throughout cellulardifferentiation" (Sauerwald, Shen and Kingsford), which apply persistent homology method to HiC data and anaylze structure changes across cell types. Specifically, the code will generate distance matrix from HiC contact matrix and use gudhi to output persistence pairs and all the simplices of the skeleton. Besides, it can be used for outputing persitence diagram, persistence barcode and other structural anaylsis. 
+This repo contains the python source code to run the method described in "Topological data analysis reveals principles of chromosome structure in cellular differentiation" (Sauerwald, Shen and Kingsford), which applies persistent homology method to HiC data and anaylze structure changes across cell types. Specifically, the code will generate distance matrix from HiC contact matrix and use gudhi to output persistence pairs and all the simplices of the skeleton. It can be used for outputing persitence diagram, persistence barcode and other structural anaylsis. The code also contains the implementation of loop trace back algorithm for generating representative loops of persistence homology modules. 
 
 
 # Dependencies
@@ -11,12 +11,17 @@ To use this python source code, you must have installed GUDHI library first. GUD
 Install GUDHI with conda: 
 conda install -c conda-forge gudhi
 
+To run loop track back algorithm, you should install NetworkX. NetworkX is a python package used for manipulating graphs and networks. You can visit the webpage (https://networkx.github.io) for more information. 
+
+Install NetworkX with conda:
+conda install -c anaconda networkx
+
 
 # Usage
 ```
 python HiC_TDA.py -i input_file_name -o output_file_name -p output_path -r resolution
 ```
-This will generate distance matrix of HiC data and output persistence pairs and all the simplices appeared during the persistent homology. 
+This will output distance matrix of HiC data, persistence pairs and all the simplices appeared during the persistent homology, and also generate loops corresponding to each persistence homology module. 
 
 Option Tag | Description
 ----------------------- | -----------------------------
@@ -27,21 +32,26 @@ Option Tag | Description
 
 Here is an example:
 ```
-python HiC_TDA.py -i example/input/RUES2_CM_combined_100000_iced_chr22.matrix -p example/output/ -o RUES2_CM_combined_100000_iced_chr22 -r 100000
+python HiC_TDA.py -i example/RUES2_CM_combined_100000_iced_chr22.matrix -p example/output/ -o RUES2_CM_combined_100000_iced_chr22 -r 100000
 ```
-Run this command line, and we will get three output files:
+Run this command line, and we will get four output files:
 
 1: RUES2_CM_combined_100000_iced_chr22_distmat.txt
 This file saves the distance matrix generated from original HiC contact matrix. 
 
 2: RUES2_CM_combined_100000_iced_chr22_persisdiagram.txt
-This file contains all the persistent diagram generated from persistent homology
+This file contains all the persistent diagram generated from persistent homology.
 - the first column: the dimension of a homology class
 - the second column: birth time
 - the third column: death time
 - the fourth column: persitence pairs
 
 3: RUES2_CM_combined_100000_iced_chr22_skeleton.txt
-This file contains all the sinmplices generated from persistent homology. 
+This file contains all the simplices generated from persistent homology. 
 - the first column: a set of nodes representing one simplex
 - the second column: the birth time of the simplex
+
+4: RUES2_CM_combined_100000_iced_chr22_loop_information.txt
+This file contains loops of all the persistence homoloy modules generated from persistent homology. Every two rows represent the information of one loop. 
+- the first row: the first column of this row is the unique edge of a persistence homology module, of which the vertices are chromosome loci. The second column is the number of edges which are different from the unique edge but have the same length. 
+- the second row: nodes of the representative loop, they are represented by chromosome loci. 
